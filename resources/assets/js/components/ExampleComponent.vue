583775e70588f846e0067b41e3f77f33f85b899f
Joel <jijoel@yahoo.com>
2017-10-25 20:00:49 +0000

    revise basic example component

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -1,23 +1,31 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
+<v-app>
+<v-container fluid>
+<v-layout row wrap justify-center>
+<v-flex xs12 md8 lg6 xl4>
 
-                    <div class="panel-body">
-                        I'm an example component!
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<v-card>
+  <v-card-title class="pb-0">
+    <h3 class="headline mb-0 primary--text">
+      Example Component
+    </h3>
+  </v-card-title>
+  <v-card-text>
+    I'm an example component!
+  </v-card-text>
+</v-card>
+
+
+</v-flex>
+</v-layout>
+</v-container>
+</v-app>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+export default {
+  created() {
+    console.log('created')
+  }
+}
 </script>
