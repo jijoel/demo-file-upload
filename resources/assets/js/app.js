cd3e6887c4d40ece5b57d5ae5bbe31f3cf077780
Joel <jijoel@yahoo.com>
2017-10-26 01:53:41 +0000

    create an avatar uploader

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -28,11 +28,17 @@ Vue.use(VeeValidate, {
     }
 })
 
+import VueClip from 'vue-clip'
+Vue.use(VueClip)
+
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+
+Vue.component('vk-avatar-uploader', require('./components/AvatarUploader.vue'))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'))
 
